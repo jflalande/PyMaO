e60b09149b65ea06a6bc687f5a52e8c8b43fc5f7
Jean-Francois Lalande <jean-francois.lalande@irisa.fr>
2019-04-10 15:17:07 +0200

    Detecting TMPFS.

--- analysis/Analysis.py
+++ analysis/Analysis.py
@@ -1,4 +1,28 @@
+
+from command_subprocessor import exec_in_subprocess
+
 class Analysis:
+
+    # The XP this analysis is part of
+    xp = None
+
+    def __init__(self, xp_):
+        self.xp = xp_
 
     def getName(self):
         return __name__
+
+    def run(self):
+        self.checkTMPFS()
+        self.analysis()
+
+    def analysis(self):
+        print("Running Apktool analysis.")
+        raise NotImplementedError("Analysis not implemented")
+
+    def checkTMPFS(self):
+        command = "mount | grep tmpfs | grep " + self.xp.TMPFS
+        print("Analysis: executing " + command)
+        errcode, res = exec_in_subprocess(command)
+        if res == "":
+            raise Exception("No tmpfs mount detected.")
