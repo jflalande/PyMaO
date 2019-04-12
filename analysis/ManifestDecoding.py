476a76e70f9f6b993a05ed6e166dbaefa939be76
Jean-Francois Lalande <jean-francois.lalande@irisa.fr>
2019-04-12 16:38:36 +0200

    Bug fix for manifest decoding (that can fail when ugly minSdkVersion are used).

--- analysis/ManifestDecoding.py
+++ analysis/ManifestDecoding.py
@@ -18,15 +18,21 @@ class ManifestDecoding(Analysis):
 
         errcode, res = self.xp.exec_in_subprocess(command, cwd=True)
 
-        for level in ["minSdkVersion", "targetSdkVersion"]:
-            matched_lines = [line for line in res.split('\n') if level in line]
-            log.debugv("Matched line: " + str(matched_lines))
-            if len(matched_lines) == 1:
-                # Converting:
-                #       A: android:minSdkVersion(0x0101020c)=(type 0x10)0x18
-                # into  24
-                SdkVersion = int(matched_lines[0].split('=')[1].split(')')[1],16)
-                self.updateJsonAnalyses(analysis_name, jsonanalyses, {level: SdkVersion})
+        try:
+            for level in ["minSdkVersion", "targetSdkVersion"]:
+                matched_lines = [line for line in res.split('\n') if level in line]
+                log.debugv("Matched line: " + str(matched_lines))
+                if len(matched_lines) == 1:
+                    # Converting:
+                    #       A: android:minSdkVersion(0x0101020c)=(type 0x10)0x18
+                    # into  24
+                    SdkVersion = int(matched_lines[0].split('=')[1].split(')')[1],16)
+                    self.updateJsonAnalyses(analysis_name, jsonanalyses, {level: SdkVersion})
+        except ValueError: # For exampe, some manifest contains 0x13 (Raw: "19"
+            log.warning("Error decoding minSdkVersion, targetSdkVersion in the Manifest for the matched lines:")
+            log.warning(str(matched_lines))
+            return False
+
 
 
         # We want to check if a specific Android version is reached
