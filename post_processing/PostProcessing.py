4dbd3ae6033dbb40650ae731f58b3d540f82ffe8
Tomás Javier Concepción Miranda <tomas.concepcion-miranda@etu.univ-rouen.fr>
2019-05-06 16:52:47 +0200

    Outputing post-processing results to xlsx
    TODO:
    - Improve post-processing speed
    - Add the possibility of resolve "complex" expressions (and, or, not)

--- post_processing/PostProcessing.py
+++ post_processing/PostProcessing.py
@@ -10,9 +10,10 @@ from os.path import isfile, join
 import BooleanParser
 
 import sys
-from pathlib import Path
+# from pathlib import Path
 
 from collections import Counter # For getting top or botton N
+from openpyxl import Workbook
 
 """
 Usage:
@@ -46,17 +47,24 @@ Example of config file:
     }
 }
 
+IDEA:
+    Add a dictionary of jsonpath values
+    Columns contents for multiexpressions:
+        1. The variable boolean expression
+        2. A dictionary of variables (the jspath expressions)
+        3. The dependency
 """
 
-outputfilename = "res.json"
+# Name without extension
+outputfilename = "res"
 
 # myjson = {'Drebin': {'date < 2013': [15, 25.862068965517242, 25.862068965517242], 'date > 2012': [43, 74.13793103448276, 74.13793103448276], '15 after 2012': [47, 81.03448275862068, 109.30232558139534], 'Popular SDK Min Version': {'15': 15, '8': 13, '14': 4, '9': 4, '7': 4}}}
-# 
+#
 # print(str(myjson))
-# 
+#
 # with open(outputfilename, 'w') as out:
 #     json.dump(myjson, out)
-# 
+#
 # quit()
 
 ################################################################################
@@ -65,32 +73,65 @@ outputfilename = "res.json"
 #                                                                              #
 ################################################################################
 
-
 def epoch_to_date(epoch):
     return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S')
 
 os.stat_float_times(False)
 
-def output_json(datasets,outfile):
+def output_to_files(datasets,jsonfile,xlsxfile):
     dico = {}
+    row_num = 1
 
-    if not os.path.isfile(outfile):
+    wb = Workbook()
+
+    # grab the active worksheet
+    ws = wb.active
+
+    # Create names for columns in workbook
+    for row_name in datasets:
+        row = datasets[row_name]
+        col_num = 2
+        for column in row.columns:
+            ws.cell(row=row_num,column=col_num,value=column.name)
+            ws.merge_cells(start_row=row_num,start_column=col_num,end_row=row_num,end_column=col_num+1)
+            col_num += 2
+
+    row_num += 1
+    col_num = 1
+
+    if not os.path.isfile(jsonfile):
         for row_name in datasets:
             # print("Assigning row " + row_name)
             row = datasets[row_name]
             dico[row_name] = {}
+            ws.cell(row=row_num,column=col_num,value=row_name)
+            col_num += 1
             for column in row.columns:
                 # print("Assiging column " + column.name)
                 column.get_total()
                 if column.req_type == None:
                     dico[row_name][column.name] = [column.total,column.pct_total,column.pct_depend]
+                    ws.cell(row=row_num,column=col_num,value=column.total)
+                    ws.cell(row=row_num+1,column=col_num,value=column.pct_total)
+                    ws.cell(row=row_num+1,column=col_num+1,value=column.pct_depend)
+                    # Grand total merge_cells
+                    ws.merge_cells(start_row=row_num,start_column=col_num,end_row=row_num,end_column=col_num+1)
                 else:
                     dico[row_name][column.name] = column.res_poll
+                col_num += 2
+            # Dataset name merge
+            ws.merge_cells(start_row=row_num,start_column=1,end_row=row_num+1,end_column=1)
+            row_num += 2
+            col_num = 1
         # print(str(dico))
 
-        print('Creating file at ' + outfile)
-        with open(outfile, 'w') as out:
+        print('Creating file at ' + jsonfile)
+        with open(jsonfile, 'w') as out:
             json.dump(dico, out)
+
+        # filename = "res.xlsx"
+        wb.save(xlsxfile)
+        print("File saved at " + xlsxfile)
     else:
         print("file already written. finishing")
 
@@ -112,15 +153,19 @@ class Column:
         self.name = name
         self.total = self.pct_total = self.pct_depend = 0
         self.parent_row = parent_row
-        self.req = req 
+        self.req = req
+        # self.req_parser = None
         self.poll = {}
         self.res_poll = {}
         # print("The req is " + str(self.req))
         # print("The length is " + str(len(self.req.split(":"))))
         if len(self.req.split(":")) < 2:
+            # Normal request
             self.req_type = None
             self.jpath = self.req.split(" ")[0]
+            self.req_parser = BooleanParser.BooleanParser(self.req)
         else:
+            # This is poll request, it only needs to retrive a value
             # self.req_type can be top or bottom
             self.jpath, self.req_type, self.num_poll = self.req.split(":")
             self.num_poll = int(self.num_poll)
@@ -130,7 +175,7 @@ class Column:
     def get_total(self):
         if self.req_type == None:
             if(self.total == 0):
-                print("The total is 0, cannot divide by 0")
+                print(self.name + ": The total is 0, cannot divide by 0")
             else:
                 self.pct_total = (self.total / self.parent_row.total)*100
                 if self.depend is None:
@@ -153,7 +198,7 @@ class Row:
     def create_column(self,name,req,depend_name=None):
         depend = None
         if depend_name != None:
-            depend = next((column for column in self.columns if column.name == depend_name), None)        
+            depend = next((column for column in self.columns if column.name == depend_name), None)
         # print('this depend is ' + str(depend))
         new_column = Column(name,self,req,depend)
         self.columns.append(new_column)
@@ -161,7 +206,8 @@ class Row:
     def process(self,json_file):
         self.total += 1
         for column in self.columns:
-            #print('parsing: ' + str(column.req))
+            # print('parsing: ' + str(column.name))
+            # for var in column.var_list:
             parse_res = parse(column.jpath).find(mwjson)
             #print("This is the result of the parsing: " + str(parse_res))
             #print()
@@ -169,7 +215,7 @@ class Row:
                 val = parse_res[0].value
                 if column.req_type == None:
                     # print('changing ' + column.jpath + ' -> ' + str(val))
-                    req = BooleanParser.BooleanParser(column.req)
+                    req = column.req_parser
                     res = req.evaluate({column.jpath:val})
                     if res:
                         column.total += 1
@@ -197,42 +243,43 @@ if not os.path.isfile(filename):
 with open(filename) as f:
     myjson = json.load(f)
 
-outfile = myjson['output_dir'] +  "/" + outputfilename
+jsonfile = myjson['output_dir'] +  "/" + outputfilename + ".json"
+xlsxfile = myjson['output_dir'] +  "/" + outputfilename + ".xlsx"
 
-if os.path.isfile(outfile):
+if os.path.isfile(jsonfile):
     print("The output file already exist. Quitting")
 else:
     datasets={} # An empty dataset (dictionary)
-    
+
     # for each row # because they are different datasets
     for row in myjson['rows']:
-    
+
     #     create row object
         datasets[row] = Row(row)
         # print()
-    
+
     #     create columns
         for column in myjson['columns']:
             # print("the column is " + str(column))
             # create_column(self,name,req,depends=None):
             datasets[row].create_column(column, myjson['columns'][column][0], myjson['columns'][column][1])
-    
+
         print()
-         
+
     #     for each file
         mypath = myjson['rows'][row]
         try:
             files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
         except:
             raise "Cannot open dir"
-        
+
         for filename in files:
             # print("Processing " + filename + " JSON file")
             with open(mypath + "/" + filename) as f:
                 mwjson = json.load(f)
-    
+
             datasets[row].process(mwjson)
-    
-    output_json(datasets,outfile)
+
+    # output_to_files(datasets,jsonfile,xlsxfile)
 
 quit()
