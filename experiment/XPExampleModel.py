cf051da1b524912ea8945c2e42cebeaa949ba286
Jean-Francois Lalande <jean-francois.lalande@irisa.fr>
2019-05-10 14:34:29 +0200

    Improved model XP.

--- experiment/XPExampleModel.py
+++ experiment/XPExampleModel.py
@@ -3,6 +3,7 @@ from experiment.Experiment import Experiment
 from analysis.Unzip import Unzip
 from analysis.GetManifAndDexDates import GetManifAndDexDates
 from analysis.ManifestDecoding import ManifestDecoding
+from analysis.AdbInstall import AdbInstall
 
 import os
 
@@ -18,13 +19,29 @@ class XPExampleModel(Experiment):
     SIMULATE_JSON_WRITE = False
 
     def __init__(self, deviceserial=None):
+        self.deviceserial = deviceserial
         self.analyses = []
 
+        # Analysis ManifestDecoding
+        # - always gives self as a parameter to an Analysis
+        # - None are the preconditions
         self.analyses.append((ManifestDecoding(self),None))
 
         # Run Unzip
+        # - No preconditions
         self.analyses.append((Unzip(self), None))
 
         # Run GetManifAndDexDates
+        # - Here the precondition is encoded as an array, containing a dictionary with:
+        #   - as key, one of the previous analysis (here Unzip)
+        #   - as value, the expected value
         self.analyses.append((GetManifAndDexDates(self),
                             [{"Unzip":{"status": "done"}}]))
+
+        # Run AdbInstall
+        # Another example:
+        # this analysis install the application if
+        #  - the ManifestDecoding succeeds
+        #  - the unzip succeeds
+        # Additionnaly, the analysis will use a device.
+        # self.analyses.append((AdbInstall(self),  [{"Unzip":{"status": "done"}}, {"ManifestDecoding": {"status": "done"}}]))
