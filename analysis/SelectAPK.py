6b253034994892e005a3a62cad7dd63a485185b6
Jean-Francois Lalande <jean-francois.lalande@irisa.fr>
2019-06-27 10:04:42 +0200

    Init for selecting APK and building a Dataset.

--- analysis/SelectAPK.py
+++ analysis/SelectAPK.py
@@ -8,10 +8,16 @@ class SelectAPK(Analysis):
 
     testStaticVariable = 0
     dateArray = {}
+    nb_apk_selected = 0
 
 
     def analysis(self, analysis, analysis_name, basename, jsonanalyses):
         log.debug("Selecting APK.")
+
+        # Upper bound for the dataset
+        if SelectAPK.nb_apk_selected >= 1000:
+            self.updateJsonAnalyses(analysis_name, jsonanalyses, {"selected": False})
+            return True
 
         log.info("Do something here Tomas :) ")
 
@@ -24,6 +30,9 @@ class SelectAPK(Analysis):
         SelectAPK.testStaticVariable = SelectAPK.testStaticVariable + 1
         log.info("For this analysis, the static variable equals " + str(SelectAPK.testStaticVariable))
 
+        # If the apk is selected
+        # if ...
+        SelectAPK.nb_apk_selected = SelectAPK.nb_apk_selected + 1
         self.updateJsonAnalyses(analysis_name, jsonanalyses, {"selected": True})
 
         # In case there is a big problem
