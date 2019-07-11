d1e2f0c7e6261f457cf471e4e2a38c59847ef9ea
Jean-Francois Lalande <jean-francois.lalande@irisa.fr>
2019-07-11 13:40:37 +0200

    Introducing CopyFile analysis.

---
+++ analysis/CopyFile.py
@@ -0,0 +1,20 @@
+
+import logging
+
+from analysis.Analysis import Analysis
+from shutil import copyfile
+
+log = logging.getLogger("orchestrator")
+
+
+class CopyFile(Analysis):
+
+    # Sending a specific parameter: the version of Android we want to check against this APK
+    def __init__(self, xp_, source=None, target=None):
+        super().__init__(xp_)
+        self.target = target
+        self.source = source
+
+    def analysis(self, analysis, analysis_name, basename, jsonanalyses):
+        log.debug("Copying File: ")
+        copyfile(self.source, self.target)
