caa69e237d520e886984b24d954c394c79745457
Tomás Javier Concepción Miranda <tomas.concepcion-miranda@etu.univ-rouen.fr>
2019-09-16 17:14:55 +0200

    Fixes in GetDEXSize analysis

--- analysis/GetDEXSize.py
+++ analysis/GetDEXSize.py
@@ -1,5 +1,6 @@
 from analysis.Analysis import Analysis
 import logging
+import time
 
 log = logging.getLogger("orchestrator")
 
@@ -9,21 +10,25 @@ class GetDEXSize(Analysis):
     def analysis(self, analysis, analysis_name, basename, jsonanalyses):
         log.debug("Running GetDEXSize analysis")
 
-        command = "unzip -l \"" + jsonanalyses["filename"] + "\" | grep ' classes.dex' | awk '{ print $1 }'"
+        command = "unzip -l \"" + jsonanalyses["filename"] + "\" 2> /dev/null | grep ' classes.dex' | awk '{ print $1 }'"
 
         errcode, res = self.xp.exec_in_subprocess(command, cwd=True)
 
+        log.debug("Errorcode: " + str(errcode))
         if errcode != 0:
-            if not res:
-                log.error("unzip command failed for " + jsonanalyses["filename"])
-                self.updateJsonAnalyses(analysis_name, jsonanalyses, {"dex_size": None})
-                return 1
-            else:
-                log.warning("unzip returned " + str(errcode))
+            # log.debug("Errorcode: " + str(errcode))
+            log.error("unzip command failed for " + jsonanalyses["filename"])
+            self.updateJsonAnalyses(analysis_name, jsonanalyses, {"dex_size": None})
+            return 1
         else:
             log.debug("unzip successful: got \'" + str(res) + "\'")
 
-        dex_size = int(res)
+        try:
+            dex_size = int(res)
+        except ValueError:
+            log.error("Something wrong happened with this APK: " + jsonanalyses['filename'])
+            self.updateJsonAnalyses(analysis_name, jsonanalyses, {"dex_size": None})
+            return 1
 
         self.updateJsonAnalyses(analysis_name, jsonanalyses, {"dex_size": dex_size})
 
